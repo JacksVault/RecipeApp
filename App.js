b62dd22c023cdd5dc14e9cbf86d9ece44be4b149
Jack Ojango <ojangoh2@outlook.com>
2023-07-31 13:20:04 +0300

    Update

--- App.js
+++ App.js
@@ -1,47 +1,65 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, Image, StyleSheet } from 'react-native';
 
 const API_KEY = '3ad313351a7449639612b18f3969f4b3';
 const API_URL = 'https://api.spoonacular.com/recipes/findByIngredients';
+const IMAGE_URL = 'https://spoonacular.com/recipeImages/';
 
 const App = () => {
-  const [ingredientInput, setIngredientInput] = useState('');
-  const [recipes, setRecipes] = useState([]);
+  const [ingredients, setIngredients] = useState('');
+  const [recipe, setRecipe] = useState(null);
+  const [error, setError] = useState('');
 
-  const handleIngredientChange = (text) => {
-    setIngredientInput(text);
-  };
-
-  const fetchRecipes = async () => {
+  const fetchRecipe = async () => {
+    setError('');
     try {
-      const response = await fetch(`${API_URL}?ingredients=${ingredientInput}&apiKey=${API_KEY}`);
+      const response = await fetch(
+        `${API_URL}?apiKey=${API_KEY}&ingredients=${encodeURIComponent(
+          ingredients
+        )}&number=1`
+      );
       const data = await response.json();
-      setRecipes(data);
+
+      if (data && data.length > 0) {
+        const recipeId = data[0].id;
+        const recipeResponse = await fetch(
+          `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${API_KEY}`
+        );
+        const detailedRecipe = await recipeResponse.json();
+        setRecipe(detailedRecipe);
+      } else {
+        setError('No recipe found for the provided ingredients.');
+      }
     } catch (error) {
-      console.error('Error fetching recipes:', error);
+      setError('Error fetching data from the server.');
     }
   };
 
-  const renderRecipeItem = ({ item }) => (
-    <TouchableOpacity style={styles.recipeItem}>
-      <Text style={styles.recipeTitle}>{item.title}</Text>
-    </TouchableOpacity>
-  );
-
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Recipe Recommendation</Text>
       <TextInput
         style={styles.input}
-        placeholder="Enter ingredients separated by commas"
-        value={ingredientInput}
-        onChangeText={handleIngredientChange}
+        placeholder="Enter your ingredients (comma-separated)"
+        value={ingredients}
+        onChangeText={(text) => setIngredients(text)}
       />
-      <Button title="Find Recipes" onPress={fetchRecipes} />
-      <FlatList
-        data={recipes}
-        renderItem={renderRecipeItem}
-        keyExtractor={(item) => item.id.toString()}
-      />
+      <Button title="Get Recipe" onPress={fetchRecipe} />
+      {recipe && (
+        <View style={styles.recipeContainer}>
+          <Text style={styles.recipeTitle}>{recipe.title}</Text>
+          {recipe.image && (
+            <Image
+              source={{ uri: `${IMAGE_URL}${recipe.image}` }}
+              style={styles.recipeImage}
+            />
+          )}
+          <Text style={styles.recipeInstructions}>
+            {recipe.instructions || 'Instructions not available.'}
+          </Text>
+        </View>
+      )}
+      {error !== '' && <Text style={styles.errorText}>{error}</Text>}
     </View>
   );
 };
@@ -49,23 +67,45 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
   },
   input: {
+    width: '100%',
     height: 40,
-    borderColor: 'gray',
     borderWidth: 1,
-    marginBottom: 16,
-    paddingHorizontal: 8,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
   },
-  recipeItem: {
-    padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: 'lightgray',
+  recipeContainer: {
+    marginTop: 30,
   },
   recipeTitle: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  recipeImage: {
+    width: 200,
+    height: 200,
+    resizeMode: 'cover',
+    marginBottom: 10,
+  },
+  recipeInstructions: {
+    fontSize: 16,
+    textAlign: 'justify',
+  },
+  errorText: {
+    color: 'red',
+    marginTop: 10,
   },
 });
 
